bf62fdf41614ccbb7e3aee350811cf76df528cca
ErikNguyen20 <20nguyened@gmail.coms>
2024-04-24 22:04:16 -0700

    Added data persistence to grocery list

--- GroceryList/GroceryItem.swift
+++ GroceryList/GroceryItem.swift
@@ -14,7 +14,7 @@ struct GroceryItem: View {
 }
 
 /// Enum that contains the categories of grocery items
-enum groceryType: String, CaseIterable {
+enum groceryType: String, CaseIterable, Codable {
     case Fruits  = "Fruits"
     case Vegtables = "Vegtables"
     case Meats = "Means"
@@ -31,11 +31,11 @@ enum groceryType: String, CaseIterable {
 }
 
 /// Grocery Item Class that encapsulates the various properties of grocery items
-class singleGroceryItem: ObservableObject, Identifiable {
-    @Published var title : String
-    @Published var description : String
-    @Published var quantity : Float
-    @Published var completed : Bool
+class singleGroceryItem: ObservableObject, Identifiable, Codable {
+    var title : String
+    var description : String
+    var quantity : Float
+    var completed : Bool
     var grocery_type : groceryType
     var price : Float?
     var id : UUID
@@ -61,10 +61,20 @@ class singleGroceryItem: ObservableObject, Identifiable {
 
 /// Grocery List Manager Class contains a list of individual grocery items and provides adding, removal, sorting, and reading operations.
 class GroceryListManager: ObservableObject {
-    @Published var myList : Array<singleGroceryItem> = []
+    @Published var myList : [singleGroceryItem] = []
+    var fileurl: URL
     
+    // Initializes the manager and also attempts to load persistent data
+    init() {
+        let directory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        self.fileurl = directory.appendingPathComponent("grocery").appendingPathExtension(".plist")
+        Load()
+    }
+    
+    // Adds an item into the grocery list. This also saves the list.
     func AddGroceryItem(_ newItem: singleGroceryItem) -> Void {
         myList.append(newItem)
+        Save()
         return
     }
     func RemoveGroceryAtIndex(_ index: Int) -> Void {
@@ -74,6 +84,23 @@ class GroceryListManager: ObservableObject {
     func SortGroceryByEnum(_ selectedGroceryEnum: groceryType) -> Array<singleGroceryItem> {
         let enumSearch = myList.filter {$0.grocery_type == selectedGroceryEnum}
         return enumSearch
+    }
+    
+    // Saves the grocery list data into an external file
+    func Save() {
+        let propertyListEncoder = PropertyListEncoder()
+        if let encodedData = try? propertyListEncoder.encode(myList) {
+            try? encodedData.write(to: self.fileurl, options: .noFileProtection)
+        }
+    }
+    
+    // Loads the grocery list data from an external file
+    func Load() {
+        let propertyListDecoder = PropertyListDecoder()
+        if let fetchedData = try? Data(contentsOf: self.fileurl),
+           let decodedData = try? propertyListDecoder.decode([singleGroceryItem].self, from: fetchedData) {
+            self.myList = decodedData
+        }
     }
 }
 
