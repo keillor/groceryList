b7fb836a499a6037cdb506937ced48e44cc3d271
Daniel Hernandez <danielhdz2000@csu.fullerton.edu>
2024-05-08 14:15:28 -0700

    Added SplashView file

---
+++ GroceryList/SplashView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashView.swift
+//  GroceryList
+//
+//  Created by Daniel Hernandez on 5/8/24.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashView()
+}
